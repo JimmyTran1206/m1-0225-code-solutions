ca33cdded4ff3eb5f9360c1270ea711b236d2c44
Jimmy Tran <quantrannhu@gmail.com>
2025-03-21 19:04:37 +0000

    Completed dom event delegation

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,13 +1,12 @@
-const $taskList = document.querySelector<HTMLElement>('.task-list');
+'use strict';
+const $taskList = document.querySelector('.task-list');
 if (!$taskList) throw new Error('Error! Cannot query task list');
-$taskList.addEventListener('click', (event: Event) => {
-  const eventTarget = event.target as HTMLElement;
+$taskList.addEventListener('click', (event) => {
+  const eventTarget = event.target;
   console.log('event target: ', eventTarget);
   console.log('event target tagName: ', eventTarget.tagName);
   if (eventTarget.tagName === 'BUTTON') {
-    const $targetParents = eventTarget.closest(
-      '.task-list-item'
-    ) as HTMLElement;
+    const $targetParents = eventTarget.closest('.task-list-item');
     if (!$targetParents)
       throw new Error('Cannot find the parent for this button!');
     console.log('closet parents: ', $targetParents);
